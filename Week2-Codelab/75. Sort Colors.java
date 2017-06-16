b019a48e374210051fe1a302c246e3a565df2be3
TravisCai <linsencai@hotmail.com>
2017-06-16 10:01:10 +0800

    Update and rename 75. Sort Colors to 75. Sort Colors.java

--- Week2-Codelab/75. Sort Colors.java
+++ Week2-Codelab/75. Sort Colors.java
@@ -79,3 +79,43 @@ Solution2:一次扫描
         
     
 }
+
+
+Solution3:计数器
+
+public class Solution {
+    public void sortColors(int[] nums) {
+        if(nums == null || nums.length == 0){
+            return;
+        }
+        
+    int zero = 0;
+    int one = 0;
+    int two = 0;
+    
+    for(int i = 0;i < nums.length;i++){
+        if(nums[i] == 0){
+            zero++;
+        }else if(nums[i] == 1){
+            one++;
+        }else if(nums[i] == 2){
+            two++;
+        }
+    }    
+        
+    for(int i =0;i < zero;i++){
+        nums[i] = 0;
+    }
+    
+    for(int i = zero;i < zero + one;i++){
+        nums[i] = 1;
+    }
+    
+    for(int i = zero + one;i < nums.length;i++){
+        nums[i] = 2;
+    }
+    
+    return;
+        
+    }
+}
